e94e2a514d74c9d5105db831f9dd3b0fee67d249
FLeiras <fleiras18@gmail.com>
2023-03-06 09:08:56 -0300

    components created

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>¿Quién es este pokémon?</h1>
+  <PokemonImage />
+  <PokemonOptions />
+</template>
+
+<script setup>
+import PokemonImage from '@/components/PokemonImage.vue'
+import PokemonOptions from '@/components/PokemonOptions'
+</script>
